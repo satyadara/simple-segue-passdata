e39b57a5a8792c5328fded0ca519c4c944164cb3
SATYADARA <satyadara69@gmail.com>
2017-08-25 16:27:28 +0700

    Picker done

--- User Interface/ViewController.swift
+++ User Interface/ViewController.swift
@@ -8,16 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     @IBOutlet weak var firstName: UITextField!
     @IBOutlet weak var lastName: UITextField!
+    @IBOutlet weak var hobbyPicker: UIPickerView!
     
+    let hobbyArray = ["Sport", "Shopping", "Gaming", "Fishing", "Coding"]
+    var selectedHobby = ""
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    /**************** SEGUE ****************/
     @IBAction func goToSecondVC(_ sender: Any) {
-        let userInput = firstName.text! + " " + lastName.text!
+        let userInput = firstName.text! + " " + lastName.text! + " " + selectedHobby
         performSegue(withIdentifier: "goToSecondVC", sender: userInput)
     }
     
@@ -28,6 +33,22 @@ class ViewController: UIViewController {
             }
         }
     }
-
+    
+    /**************** PICKER ****************/
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return hobbyArray[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return hobbyArray.count
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        selectedHobby = hobbyArray[row]
+    }
 }
 
